dd9dddbe9720410d5a39143ec497da4b8142a798
itog <itog@itog-lab.info>
2009-11-09 11:36:14 +0900

    first commit

---
+++ src/org/itog/playground/Playground.java
@@ -0,0 +1,13 @@
+package org.itog.playground;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Playground extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
